8b7c6865651499c3f21b9824a834f33831c0fdf5
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-08 20:32:20 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -98,7 +98,10 @@ server.put('/api/dogs/:id', async (req, res) => {
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 server.delete('/api/dogs/:id', async (req, res) => {
   try {
-    const result 
+    const deleted = await Dog.delete()
+    if (!deleted) {
+      
+    }
   } catch(err) {
     res.status(500).json({ message: err.message })
   }
