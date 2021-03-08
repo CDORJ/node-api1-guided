f56858673cf5cfc89dc8513efb34e5891f640c82
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-08 19:55:26 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ server.get('/api/dogs', (req, res) => {
   Dog.findAll()
     .then(dogs => {
       console.log(dogs)
-      
+      res.status(200).json(dogs)
     })
     .catch(err => {
       res.status(500).json({ message: err.message })
