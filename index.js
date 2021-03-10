00d0fcb8122f4ba5dc79a0d7f27649c09d7a648b
ORiveraJr84 <oriverajr84@gmail.com>
2021-03-09 23:55:32 -0500

    WIP: Working through lecture

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // import the server and start it
+const server = require("./api/server");
+
+const PORT = 3000;
+
+server.listen(PORT,()=> {
+    console.log(`\n *** Listening on port ${PORT} *** \n`)
+})
