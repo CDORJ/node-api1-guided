9caf32da990f23568c4f484a9ed678a01cc31cdf
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-06 09:44:29 -0400

    Initial commit with some work done.

--- api/server.js
+++ api/server.js
@@ -1,31 +1,46 @@
 // IMPORTS AT THE TOP
-// IMPORTS AT THE TOP
-// IMPORTS AT THE TOP
+const express = require("express");
+const Dog = require("./dog-model");
 
 // INSTANCE OF EXPRESS APP
-// INSTANCE OF EXPRESS APP
-// INSTANCE OF EXPRESS APP
+const server = express();
 
 // GLOBAL MIDDLEWARE
-// GLOBAL MIDDLEWARE
-// GLOBAL MIDDLEWARE
+server.use(express.json);
 
 // ENDPOINTS
 // ENDPOINTS
 // ENDPOINTS
 
 // [GET] / (Hello World endpoint)
+server.get("/", (req, res) => {
+  res.status(200);
+  res.send({ message: "Hello World" });
+});
 
 // [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
+server.get("/api/dogs", async (req, res) => {
+  const dogs = await Dog.findAll();
+  res.send(dogs);
+});
 
 // [GET] /api/dogs (R of CRUD, fetch all dogs)
 
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
+server.post("/api/dogs", async (req, res) => {
+  const { name, weight } = req.body;
+
+  if (!name || !weight) {
+    res.status(400).send({ message: "name and weight required" });
+  } else {
+    const newDog = await Dog.create({ name, weight });
+    res.status(200).send(newDog);
+  }
+});
 
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
-// EXPOSING THE SERVER TO OTHER MODULES
-// EXPOSING THE SERVER TO OTHER MODULES
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // import the server and start it
+const server = require("./api/server");
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
